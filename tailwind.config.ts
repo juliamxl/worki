a43e1da7043b4dac87d9636b291654f91febd762
juyyaa <juliaamarxal@gmail.com>
2023-10-02 19:19:48 -0300

    atualizando fonte

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -16,13 +17,8 @@ const config: Config = {
     },
 
     fontFamily: {
-      sans: [
-        'Inter var, sans-serif',
-        {
-          fontFeatureSettings: '"cv11", "ss01"',
-          fontVariationSettings: '"opsz" 32',
-        },
-      ],
+      sans: 'var(--font-roboto)',
+      alt: 'var(--font-bai-janjuree)',
     },
   },
   plugins: [],
